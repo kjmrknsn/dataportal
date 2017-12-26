a831201cdbd491cc3e16a9108d308687470f3779
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-26 22:17:27 +0900

    Update

--- ddl/directory.sql
+++ ddl/directory.sql
@@ -1,17 +1,14 @@
-create table space_member (
+create table directory (
   id bigint unsigned not null auto_increment
 , space_id bigint unsigned not null
-, uid varchar(64) not null
-, is_writer bool not null default false
+, name varchar(64) not null
+, creator_uid varchar(64) not null
+, updater_uid varchar(64) not null
 , created_at timestamp not null default current_timestamp
 , updated_at timestamp not null default current_timestamp on update current_timestamp
 , primary key (id)
 );
 
-alter table space_member add index space_member_0001 (
+alter table directory add index directory_0001 (
   space_id
-);
-
-alter table space_member add index space_member_0002 (
-  uid
 );
--- ddl/space.sql
+++ ddl/space.sql
@@ -3,6 +3,6 @@ create table space (
 , name varchar(64) not null
 , spark_submit_proxy_user varchar(64) not null
 , created_at timestamp not null default current_timestamp
-, updated_at timestamp not null default current_timestamp
+, updated_at timestamp not null default current_timestamp on update current_timestamp
 , primary key (id)
 )
