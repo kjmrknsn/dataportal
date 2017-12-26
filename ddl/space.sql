66a6e88cff1c1ab61b8b7c6fcca067825efa4775
Keiji Yoshida <kjmrknsn@gmail.com>
2017-12-26 21:33:46 +0900

    Update

---
+++ ddl/space.sql
@@ -0,0 +1,8 @@
+create table space (
+  id bigint unsigned not null auto_increment
+, name varchar(64) not null
+, spark_submit_proxy_user varchar(64) not null
+, created_at timestamp not null default current_timestamp
+, updated_at timestamp not null default current_timestamp
+, primary key (id)
+)
